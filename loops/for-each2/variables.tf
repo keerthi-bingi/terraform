e9008c1c46338fa15b5d55ff477d47a55de88328
keerthi-bingi <keerthibingi1312@gmail.com>
2026-02-26 17:14:00 +0530

    Adding for each loop

---
+++ loops/for-each2/variables.tf
@@ -0,0 +1,15 @@
+# This should be converted into set
+variable "instances" {
+    type = list
+    default = ["mongodb", "redis"]
+}
+
+
+# replace with your zone_id and domain_name
+variable "zone_id" {
+    default = "Z031668720LL13SP2V0EM"
+}
+
+variable "domain_name" {
+    default = "bingi.online"
+}
